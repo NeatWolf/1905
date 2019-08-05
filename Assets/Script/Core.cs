15ff84db8d828c7b6ae1fca5a553434e7dd1afc7
JASON <1312119064@qq.com>
2019-08-06 00:13:27 +0800

    Merge branch 'master' of https://github.com/qq1312119064/1905

--- Assets/Script/Core.cs
+++ Assets/Script/Core.cs
@@ -30,7 +30,7 @@ public class Core : MonoBehaviour
         DontDestroyOnLoad(GameObject.Find("UI"));
         DontDestroyOnLoad(GameObject.Find("Core"));
 
-        table = Luax.Instance.DoString("require('Core.lua.txt')").Get<LuaTable>("Core");
+        table = Luax.Instance.DoString("require('Lua/Core.lua.txt')").Get<LuaTable>("Core");
         callLua.start = table.Get<UnityAction>("Start");
         callLua.update = table.Get<UnityAction>("Update");
         callLua.SecondUpdate = table.Get<UnityAction>("SecondUpdate");

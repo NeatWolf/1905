cec1c9438fbe504d689699369fb743f17648d8a6
AYun-Neo <1096344703@qq.com>
2019-08-15 11:39:57 +0800

    ui

--- Assets/Script/CSharp/Animation/AnimateManager.cs
+++ Assets/Script/CSharp/Animation/AnimateManager.cs
@@ -67,11 +67,12 @@ public static class AnimateManager
 
     }
 
-    /**
-    RoleMan出售记录入场动画
-    records--出售记录列表
-    content--卷轴内的content
-     */
+
+    /// <summary>
+    /// RoleMan出售记录入场动画
+    /// </summary>
+    /// <param name="records">出售记录列表</param>
+    /// <param name="content">卷轴内的content</param>
     public static void RecordEnterAnimate(GameObject records, GameObject content)
     {
         records.gameObject.SetActive(true);
@@ -101,9 +102,11 @@ public static class AnimateManager
 
 
     }
-    /**
-    RoleMan出售记录失活状态下位置设置
-     */
+
+    /// <summary>
+    /// RoleMan出售记录失活状态下位置设置
+    /// </summary>
+    /// <param name="records"></param>
     public static void RecordPreviousAnimate(GameObject records)
     {
         records.gameObject.SetActive(false);
@@ -111,30 +114,34 @@ public static class AnimateManager
     }
 
 
-    /*
-    Lottery UI 入场动画
 
-     */
+    /// <summary>
+    /// Lottery UI 入场动画
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <param name="led"></param>
+    /// <param name="halo"></param>
     public static void LotteryEnterAnimate(GameObject left, GameObject right, GameObject led, GameObject halo)
     {
         left.gameObject.SetActive(true);
         right.gameObject.SetActive(true);
         led.gameObject.SetActive(true);
         halo.gameObject.SetActive(true);
-        led.GetComponent<Image>().color=new Color(1,1,1,0);
-        
+        led.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+
 
         //left动画
         left.transform.DOLocalMoveX(-290, 1).SetEase(Ease.InOutBack);
-        Sequence seqLeft=DOTween.Sequence(); 
+        Sequence seqLeft = DOTween.Sequence();
         seqLeft.Append(left.transform.DORotate(new Vector3(-60, 0, 0), 0.5f));
         seqLeft.Append(left.transform.DORotate(new Vector3(40, 0, 0), 0.3f));
         seqLeft.Append(left.transform.DORotate(new Vector3(-20, 0, 0), 0.3f));
         seqLeft.Append(left.transform.DORotate(new Vector3(0, 0, 0), 0.1f));
-       
+
         //right动画
         right.transform.DOLocalMoveX(300, 1).SetEase(Ease.InOutBack);
-        Sequence seqRight=DOTween.Sequence(); 
+        Sequence seqRight = DOTween.Sequence();
         seqRight.Append(right.transform.DORotate(new Vector3(-60, 0, 0), 0.5f));
         seqRight.Append(right.transform.DORotate(new Vector3(40, 0, 0), 0.3f));
         seqRight.Append(right.transform.DORotate(new Vector3(-20, 0, 0), 0.3f));
@@ -147,9 +154,14 @@ public static class AnimateManager
 
     }
 
-    /*
-    Lottery失活状态下位置设置
-     */
+
+    /// <summary>
+    /// Lottery失活状态下位置设置
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <param name="led"></param>
+    /// <param name="halo"></param>
     public static void LotteryPreviousAnimate(GameObject left, GameObject right, GameObject led, GameObject halo)
     {
         //  left.gameObject.SetActive(false);
@@ -158,22 +170,126 @@ public static class AnimateManager
         halo.gameObject.SetActive(false);
         left.transform.localPosition = new Vector3(-700, left.transform.localPosition.y, left.transform.localPosition.z);
         right.transform.localPosition = new Vector3(700, right.transform.localPosition.y, right.transform.localPosition.z);
-        
+
 
     }
 
-    /*
-    探索界面的编队界面入场动画
-     */
-    public static void TroopsEnterAnimate(){}
 
-    /* 
-    探索界面的编队界面失活状态下位置设置
-    */
-    public static void TroopsPreviousnimate(GameObject troops,GameObject bg,GameObject title){
+
+
+    /// <summary>
+    /// 探索界面的编队界面入场动画
+    /// </summary>
+    /// <param name="troops"></param>
+    /// <param name="bg"></param>
+    /// <param name="title"></param>
+    /// <param name="heng"></param>
+    /// <param name="heng2"></param>
+    /// <param name="heng3"></param>
+    /// <param name="toggleSwitch"></param>
+    /// <param name="scroll"></param>
+    /// <param name="heng4"></param>
+    /// <param name="troopsGroup"></param>
+    /// <param name="slider"></param>
+    /// <param name="mainTitle"></param>
+    /// <param name="info"></param>
+    /// <param name="icon"></param>
+    public static void TroopsEnterAnimate(GameObject troops, GameObject bg, GameObject title, GameObject heng, GameObject heng2,
+     GameObject heng3, GameObject toggleSwitch, GameObject scroll, GameObject heng4,
+     GameObject troopsGroup, GameObject slider, GameObject mainTitle, GameObject info, GameObject icon)
+    {
+
+        mainTitle.transform.DOBlendableLocalMoveBy(new Vector3(-300, 0, 0), 1).SetEase(Ease.InOutBack);
+        slider.transform.DOBlendableLocalMoveBy(new Vector3(300, 0, 0), 1).SetEase(Ease.InOutBack);
+        troopsGroup.transform.DOLocalRotate(new Vector3(0, 0, -90), 0.5f).SetEase(Ease.InOutBack);
+        troops.SetActive(true);
+        // troops.transform.DOScale(Vector3.one,0.5f);
+        //troops.transform.DOLocalMove(Vector3.one,0.5f);
+        //bg.GetComponent<Image>().color=new Color(0.1803922f,0.01568628f,0.2666667f,0.4f);
+        bg.GetComponent<Image>().DOFade(0.4f, 1);
+        title.transform.DOLocalMoveX(170f, 1).SetEase(Ease.InOutBack);
+        heng.transform.DOLocalMoveX(0, 2).SetEase(Ease.InOutBack);
+        heng2.transform.DOLocalMoveX(0, 1.5f).SetEase(Ease.InOutBack);
+        heng3.transform.DOLocalMoveX(0, 1.5f).SetEase(Ease.InOutBack);
+        toggleSwitch.transform.DOLocalMoveX(0, 1.5f).SetEase(Ease.InOutBack);
+        scroll.transform.DOLocalMoveX(0, 2).SetEase(Ease.InOutBack);
+        heng4.transform.DOLocalMoveX(0, 2).SetEase(Ease.InOutBack);
+        info.transform.DOLocalMoveX(0, 1.2f).SetEase(Ease.InOutBack);
+        icon.transform.DOLocalMoveX(0, 1.5f).SetEase(Ease.InOutBack);
+
+    }
+
+
+    /// <summary>
+    /// 探索界面的编队界面失活状态下位置设置
+    /// </summary>
+    /// <param name="troops"></param>
+    /// <param name="bg"></param>
+    /// <param name="title"></param>
+    /// <param name="heng"></param>
+    /// <param name="heng2"></param>
+    /// <param name="heng3"></param>
+    /// <param name="btnTroops"></param>
+    /// <param name="toggleSwitch"></param>
+    /// <param name="scroll"></param>
+    /// <param name="heng4"></param>
+    /// <param name="info"></param>
+    /// <param name="icon"></param>
+    public static void TroopsPreviousnimate(GameObject troops, GameObject bg, GameObject title, GameObject heng, GameObject heng2, GameObject heng3, Button btnTroops, GameObject toggleSwitch, GameObject scroll, GameObject heng4, GameObject info, GameObject icon)
+    {
+
         troops.SetActive(false);
-        bg.GetComponent<Image>().color=new Color(0.1803922f,0.01568628f,0.2666667f,0);
-        title.transform.localPosition=new Vector3(1000,);
+
+
+        bg.GetComponent<Image>().color = new Color(0.1803922f, 0.01568628f, 0.2666667f, 0);
+        bg.transform.localScale = new Vector3(1000, 1000, 1000);
+
+        title.transform.localPosition = new Vector3(1000, 699.1f, 0);
+        heng.transform.localPosition = new Vector3(1500, heng.transform.localPosition.y, heng.transform.localPosition.z);
+        heng2.transform.localPosition = new Vector3(1500, heng2.transform.localPosition.y, heng2.transform.localPosition.z);
+        heng3.transform.localPosition = new Vector3(1500, heng3.transform.localPosition.y, heng3.transform.localPosition.z);
+        heng4.transform.localPosition = new Vector3(1500, heng4.transform.localPosition.y, heng4.transform.localPosition.z);
+        toggleSwitch.transform.localPosition = new Vector3(1500, toggleSwitch.transform.localPosition.y, toggleSwitch.transform.localPosition.z);
+
+        scroll.transform.localPosition = new Vector3(1500, scroll.transform.localPosition.y, scroll.transform.localPosition.z);
+        info.transform.localPosition = new Vector3(1500, info.transform.localPosition.y, info.transform.localPosition.z);
+        icon.transform.localPosition = new Vector3(1500, icon.transform.localPosition.y, icon.transform.localPosition.z);
+    }
+    /// <summary>
+    /// 探索界面编队界面退出动画
+    /// </summary>
+    /// <param name="troops"></param>
+    /// <param name="bg"></param>
+    /// <param name="title"></param>
+    /// <param name="heng"></param>
+    /// <param name="heng2"></param>
+    /// <param name="heng3"></param>
+    /// <param name="toggleSwitch"></param>
+    /// <param name="scroll"></param>
+    /// <param name="heng4"></param>
+    /// <param name="troopsGroup"></param>
+    /// <param name="slider"></param>
+    /// <param name="mainTitle"></param>
+    /// <param name="info"></param>
+    /// <param name="icon"></param>
+    public static void TroopsExitAnimate(GameObject troops, GameObject bg, GameObject title, GameObject heng, GameObject heng2,
+     GameObject heng3, GameObject toggleSwitch, GameObject scroll, GameObject heng4,
+     GameObject troopsGroup, GameObject slider, GameObject mainTitle, GameObject info, GameObject icon)
+    {
+
+        troopsGroup.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.5f).SetEase(Ease.InOutBack).SetDelay(1.5f);
+
+        bg.GetComponent<Image>().DOFade(0f, 1);
+        title.transform.DOLocalMoveX(1000, 1).SetEase(Ease.InOutBack);
+        heng.transform.DOMoveX(1500, 2).SetEase(Ease.InOutBack);
+        heng2.transform.DOMoveX(1500, 1.5f).SetEase(Ease.InOutBack);
+        heng3.transform.DOMoveX(1500, 1.5f).SetEase(Ease.InOutBack);
+        toggleSwitch.transform.DOLocalMoveX(1500, 1.5f).SetEase(Ease.InOutBack);
+        scroll.transform.DOLocalMoveX(1500, 2).SetEase(Ease.InOutBack);
+        heng4.transform.DOLocalMoveX(1500, 2).SetEase(Ease.InOutBack);
+        info.transform.DOLocalMoveX(1500, 1.2f).SetEase(Ease.InOutBack);
+        icon.transform.DOLocalMoveX(1500, 1.5f).SetEase(Ease.InOutBack);
+
 
     }
 
---
+++ Assets/Script/CSharp/Animation/ExploreDragAnimate.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ExploreDragAnimate : MonoBehaviour, IDragHandler, IBeginDragHandler,
+IEndDragHandler, IPointerClickHandler, IPointerExitHandler, IPointerUpHandler
+{
+    [HideInInspector]
+    public Vector3 offset;
+    Vector3 worldPos;
+    Camera mainCam;
+    ExploreAnimate ea;
+    Button[] sceneBtns;
+    [Header("滑动惯行")]
+    public float inertia;
+    
+
+
+    private void Awake()
+    {
+        ea = GameObject.Find("UI/ExploreUI").GetComponent<ExploreAnimate>();
+
+        mainCam = GameObject.Find("Scene/Explore/Main Camera").GetComponent<Camera>();
+        sceneBtns = GameObject.Find("Scene/Explore/CityPlane/Canvas").GetComponent<UISubObject>().buttons;
+
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        ea.isDarp = true;
+        
+
+    }
+   
+
+    //场景正在向前运动或向后运动，1：向前，-1：向后
+    int forwardORback;
+   
+    /// <summary>
+    /// 屏幕滑动
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        //     if (RectTransformUtility.ScreenPointToWorldPointInRectangle(transform.parent as RectTransform, eventData.position
+        //  , eventData.pressEventCamera, out worldPos))
+        //     {
+        //         worldPos=new Vector3(worldPos.x,worldPos.y-1000,worldPos.z);
+        //         offset = transform.position - worldPos;
+
+        //         Debug.Log("offset"+offset);
+        //         mainCam.transform.Translate(0,0,-(offset.y-1000)*0.01f,Space.World);
+        //     }
+        
+        
+        
+        
+        float k = offset.y;
+        offset = eventData.pressEventCamera.ScreenToWorldPoint(eventData.position - transform.GetComponent<RectTransform>().offsetMin);
+
+
+        if (k > offset.y && mainCam.transform.localPosition.z < 10f)
+        {
+            forwardORback = 1;
+            mainCam.transform.Translate(0, 0, offset.y * 0.001f, Space.World);
+
+
+        }
+        else if (k < offset.y && mainCam.transform.localPosition.z > -25f)
+        {
+            forwardORback = -1;
+            mainCam.transform.Translate(0, 0, -offset.y * 0.001f, Space.World);
+        }
+
+    
+   
+    }
+    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+        //惯行滑动
+        if (mainCam.transform.localPosition.z > -21 && mainCam.transform.localPosition.z < -2)
+        {
+            
+            if (forwardORback == 1)
+            {
+               
+                mainCam.transform.DOLocalMoveZ(mainCam.transform.localPosition.z + inertia, 1).SetEase(Ease.OutQuad).OnComplete(() =>
+               {
+                   
+                   if (mainCam.transform.localPosition.z < -21)
+                   {
+                       Tweener t = mainCam.transform.DOLocalMoveZ(-21, 1);
+                       t.OnComplete(() =>
+                     {
+                         ea.isDarp = false;
+
+                     });
+
+                   }
+                   else if (mainCam.transform.localPosition.z > -2)
+                   {
+                       Tweener t = mainCam.transform.DOLocalMoveZ(-2, 1);
+                       t.OnComplete(() =>
+                     {
+                         ea.isDarp = false;
+                         if (mainCam.transform.localPosition.z < -21)
+                         {
+                             Tweener t1 = mainCam.transform.DOLocalMoveZ(-21, 1);
+                             t.OnComplete(() =>
+                           {
+                               ea.isDarp = false;
+
+                           });
+
+                         }
+                         else if (mainCam.transform.localPosition.z > -2)
+                         {
+                             Tweener t1 = mainCam.transform.DOLocalMoveZ(-2, 1);
+                             t.OnComplete(() =>
+                           {
+                               ea.isDarp = false;
+
+                           });
+
+                         }
+                         else
+                         {
+                             ea.isDarp = false;
+                         }
+
+                     });
+
+                   }
+                   else
+                   {
+                       ea.isDarp = false;
+                   }
+
+               });
+                
+            }
+            else if (forwardORback == -1)
+            {
+                mainCam.transform.DOLocalMoveZ(mainCam.transform.localPosition.z - inertia, 1).SetEase(Ease.OutQuad).OnComplete(() =>
+              {
+                  
+
+                  
+                  if (mainCam.transform.localPosition.z < -21)
+                  {
+                      Tweener t = mainCam.transform.DOLocalMoveZ(-21, 1);
+                      t.OnComplete(() =>
+                    {
+                        ea.isDarp = false;
+
+                    });
+
+                  }
+                  else if (mainCam.transform.localPosition.z > -2)
+                  {
+                      Tweener t = mainCam.transform.DOLocalMoveZ(-2, 1);
+                      t.OnComplete(() =>
+                    {
+                        ea.isDarp = false;
+                        if (mainCam.transform.localPosition.z < -21)
+                        {
+                            Tweener t1 = mainCam.transform.DOLocalMoveZ(-21, 1);
+                            t.OnComplete(() =>
+                          {
+                              ea.isDarp = false;
+
+                          });
+
+                        }
+                        else if (mainCam.transform.localPosition.z > -2)
+                        {
+                            Tweener t1 = mainCam.transform.DOLocalMoveZ(-2, 1);
+                            t.OnComplete(() =>
+                          {
+                              ea.isDarp = false;
+
+                          });
+
+                        }
+                        else
+                        {
+                            ea.isDarp = false;
+                        }
+
+                    });
+
+                  }
+                  else
+                  {
+                      ea.isDarp = false;
+                  }
+
+              });
+            
+            }
+        
+        }
+                //分割
+        else if (mainCam.transform.localPosition.z < -21)
+        {
+            Tweener t = mainCam.transform.DOLocalMoveZ(-21, 1);
+            t.OnComplete(() =>
+            {
+                ea.isDarp = false;
+
+            });
+
+        }
+        else if (mainCam.transform.localPosition.z > -2)
+        {
+            Tweener t = mainCam.transform.DOLocalMoveZ(-2, 1);
+            t.OnComplete(() =>
+            {
+                ea.isDarp = false;
+
+            });
+
+        }
+        else
+        {
+            ea.isDarp = false;
+        }
+
+
+    }
+    Vector3 mouseOffset;
+    Tweener bt0;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //方法一
+
+        // Debug.Log("eventData.pressPosition"+eventData.pressPosition);
+        // //btnCamoffset--第一个button与场景中的mainCam的Z轴差值
+        // float btnCamoffset = GameObject.Find("Scene/Explore/CityPlane/Canvas").GetComponent<UISubObject>().buttons[0].transform.position.z - mainCam.transform.position.z;
+        // Debug.Log("button" + btnCamoffset);
+        // if(btnCamoffset>9.3f&&eventData.pressPosition.y<1100&&eventData.pressPosition.y>578){
+        //     sceneBtns[0].transform.DOScale(2,0.5f);
+        // }
+        // else if((btnCamoffset>9.3f &&eventData.pressPosition.y>1100&&eventData.pressPosition.y<595)||(btnCamoffset>-1.74f&& btnCamoffset<8.8f&&eventData.pressPosition.y>500)){
+        //     sceneBtns[1].transform.DOScale(2,0.5f);
+        // }
+
+        // if (btnCamoffset>9.3f&&eventData.pressPosition.y>1100||eventData.pressPosition.y<578)
+        // {
+        //     sceneBtns[0].transform.DOScale(1,0.5f);
+        // }
+
+        //方法二
+
+
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+}

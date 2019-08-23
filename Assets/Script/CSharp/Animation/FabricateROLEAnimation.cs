94a1edd13b03c2bf1e7c6cd9fa9808fc0301646e
AYun-Neo <1096344703@qq.com>
2019-08-23 21:46:50 +0800

    ui

--- Assets/Script/CSharp/Animation/FabricateROLEAnimation.cs
+++ Assets/Script/CSharp/Animation/FabricateROLEAnimation.cs
@@ -6,31 +6,35 @@ using DG.Tweening;
 
 public class FabricateROLEAnimation : MonoBehaviour
 {
-    GameObject sroll, choice, bg;
+    GameObject sroll, choice, bg, content,Window;
     private void Awake()
     {
         sroll = GetComponent<UISubObject>().go[1];
         choice = GetComponent<UISubObject>().go[2];
         bg = GetComponent<UISubObject>().go[3];
+        content = GetComponent<UISubObject>().go[0];
+        Window=GetComponent<UISubObject>().go[4];
     }
-   
+
 
     public void ROLEEnterAnimate()
     {
         gameObject.SetActive(true);
-        gameObject.transform.GetChild(0).GetComponent<RectTransform>().DOAnchorPosX(gameObject.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition.x+10, 0.7f).From().onComplete=()=>{
-            bg.GetComponent<Image>().DOFade(0.4f, 0.5f);
-        };
-        
+        bg.SetActive(true);
+        bg.GetComponent<Image>().DOFade(0.4f, 0.5f);
+        Window.GetComponent<RectTransform>().DOAnchorPosX(Window.GetComponent<RectTransform>().anchoredPosition.x + 200, 0.7f).From();
+
     }
     public void ROLEExitAnimate()
     {
         bg.GetComponent<Image>().DOFade(0f, 0.5f);
-        gameObject.transform.GetChild(0).GetComponent<RectTransform>().DOAnchorPosX(gameObject.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition.x+10,1f).onComplete=()=>{
+        Window.GetComponent<RectTransform>().DOAnchorPosX(Window.GetComponent<RectTransform>().anchoredPosition.x + 200, 0.7f).onComplete = () =>
+        {
+            Window.transform.localPosition -= new Vector3(200, 0, 0);
             gameObject.SetActive(false);
-            gameObject.transform.GetChild(0).transform.localPosition-=new Vector3(10,0,0);
+            bg.SetActive(false);
         };
-        
-        
+
+
     }
 }

9ba52b9050e230b3eb40b4ecfe335f0c24b16ce4
AYun-Neo <1096344703@qq.com>
2019-08-24 11:37:22 +0800

    all

--- Assets/Script/CSharp/Animation/BtnAnimate.cs
+++ Assets/Script/CSharp/Animation/BtnAnimate.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+//
 public class BtnAnimate : MonoBehaviour,
 IPointerDownHandler, IPointerUpHandler,
 IPointerEnterHandler, IPointerExitHandler,

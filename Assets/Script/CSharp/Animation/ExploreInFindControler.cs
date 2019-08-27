bc4031a5c82f3f0592870f3d54dcb0e525b4b047
AYun-Neo <1096344703@qq.com>
2019-08-27 16:18:52 +0800

    相机跟踪与怪物生成

--- Assets/Script/CSharp/Animation/ExploreInFindControler.cs
+++ Assets/Script/CSharp/Animation/ExploreInFindControler.cs
@@ -8,6 +8,21 @@ using DG.Tweening;
 
 public class ExploreInFindControler : MonoBehaviour
 {
+    [Header("怪物出生点")]
+    /// <summary>
+    /// 怪物出生点
+    /// </summary>
+    public GameObject[] MonsterSpawnPos = new GameObject[18];
+    [Header("怪物")]
+    /// <summary>
+    /// 怪物
+    /// </summary>
+    public GameObject[] mapMonster = new GameObject[8];
+    [Header("地图编号，依靠此数值决定生成怪物的数值")]
+    /// <summary>
+    /// 地图编号，依靠此数值决定生成怪物的数值
+    /// </summary>
+    public int MapNumber = 0;
     NavMeshAgent mAgent;
     GameObject hero, monster, camera, relaCamera;
     UnityArmatureComponent uac;
@@ -43,7 +58,10 @@ public class ExploreInFindControler : MonoBehaviour
     public UnityAction LevelFourMonster;
 
 
-    //英雄攻击距离，即寻路停止时与目标的距离
+    [Header("英雄攻击距离，即寻路停止时与目标的距离")]
+    /// <summary>
+    /// 英雄攻击距离，即寻路停止时与目标的距离
+    /// </summary>
     public int heroAtkDistance = 2;
     Vector3 targetPos;
     /// <summary>
@@ -60,11 +78,17 @@ public class ExploreInFindControler : MonoBehaviour
     //相机是否跟随英雄
     bool cameraTrack = true;
 
-
-
+    /// <summary>
+    /// 复制出的怪物，int为地图编号，list为怪物
+    /// </summary>
+    Dictionary<int, List<GameObject>> monsterDic = new Dictionary<int, List<GameObject>>();
+    /// <summary>
+    /// 怪物池
+    /// </summary>
+    public GameObject monsterPool;
     private void Awake()
     {
-
+        
         monster = GetComponent<UISubObject>().go[0];
         camera = GetComponent<UISubObject>().go[2];
         relaCamera = GetComponent<UISubObject>().go[2].transform.GetChild(0).gameObject;
@@ -75,7 +99,12 @@ public class ExploreInFindControler : MonoBehaviour
             heroAgent[i] = heroTroop[i].GetComponent<NavMeshAgent>();
         }
 
+        //根据地图编号分配怪物出生点
 
+        SpawnMonster(MapNumber);
+
+        Quaternion quaternion = Quaternion.LookRotation((heroTroop[0].transform.position - camera.transform.GetChild(0).transform.position));
+        camera.transform.GetChild(0).transform.rotation = Quaternion.Lerp(camera.transform.GetChild(0).transform.rotation, quaternion, 0.5f);
 
     }
 
@@ -154,7 +183,7 @@ public class ExploreInFindControler : MonoBehaviour
             if (Vector3.Distance(heroTroop[i].transform.position, targetPos) <= heroAtkDistance)
             {
                 Debug.Log("播放idle动画");
-               // isIdle = true;
+                // isIdle = true;
                 herosUAC[i].animation.Play("idle");
                 //调用遇怪方法
                 if (CurrentMonster != null)
@@ -178,8 +207,9 @@ public class ExploreInFindControler : MonoBehaviour
                         if (LevelThreeMonster == null) return;
                         LevelThreeMonster();
                     }
-                    else if(CurrentMonster.name=="07"||CurrentMonster.name=="08"){
-                        if(LevelFourMonster==null) return;
+                    else if (CurrentMonster.name == "07" || CurrentMonster.name == "08")
+                    {
+                        if (LevelFourMonster == null) return;
                         LevelFourMonster();
                     }
 
@@ -191,10 +221,31 @@ public class ExploreInFindControler : MonoBehaviour
         }
 
 
+        //怪物看向摄像机
+        if (monsterDic.ContainsKey(MapNumber))
+        {
+            for (int i = 0; i < 18; i++)
+            {
+
+                monsterDic[MapNumber][i].transform.LookAt(relaCamera.transform.position);
+            }
+        }
 
 
 
 
+    }
+    
+    //相机跟随
+    private void FixedUpdate() {
+        if ((cameraTrack && heroTroop[0] != null))
+        {
+
+            // Quaternion quaternion = Quaternion.LookRotation((heroTroop[0].transform.position - camera.transform.GetChild(0).transform.position));
+            // relaCamera.transform.rotation = Quaternion.Lerp(camera.transform.GetChild(0).transform.rotation, quaternion, 0.5f);
+            // camera.transform.GetChild(0).transform.position = new Vector3(heroTroop[0].transform.position.x + (relaCamera.transform.position.x - heroTroop[0].transform.position.x),heroTroop[0].transform.position.y + (relaCamera.transform.position.y - heroTroop[0].transform.position.y), camera.transform.GetChild(0).transform.position.z);
+            camera.transform.localPosition =heroTroop[0].transform.position;
+        }
 
     }
 
@@ -207,14 +258,7 @@ public class ExploreInFindControler : MonoBehaviour
             cameraTrack = true;
         }
 
-        if ((cameraTrack && heroTroop[0] != null))
-        {
-
-            Quaternion quaternion = Quaternion.LookRotation((heroTroop[0].transform.position - camera.transform.GetChild(0).transform.position));
-            camera.transform.GetChild(0).transform.rotation = Quaternion.Lerp(camera.transform.GetChild(0).transform.rotation, quaternion, 0.5f);
-            camera.transform.GetChild(0).transform.position = new Vector3(heroTroop[0].transform.position.x + (relaCamera.transform.position.x - heroTroop[0].transform.position.x), camera.transform.GetChild(0).transform.position.y, camera.transform.GetChild(0).transform.position.z);
-
-        }
+        
 
 
 
@@ -225,12 +269,12 @@ public class ExploreInFindControler : MonoBehaviour
             switch (cameraMoveType)
             {
                 case 1:
-                    // relaCamera.transform.Translate(0, Input.GetAxis("Vertical") * 0.1f, 0);
-                    CameraMove(0, Input.GetAxis("Vertical") * 0.1f, 0);
+
+                    CameraMove( Input.GetAxis("Vertical") * 0.1f,0,0);
                     break;
                 case 2:
-                    //  relaCamera.transform.Translate(Input.GetAxis("Horizontal") * 0.1f, 0, 0);
-                    CameraMove(Input.GetAxis("Horizontal") * 0.1f, 0, 0);
+
+                    CameraMove( 0, 0,-Input.GetAxis("Horizontal") * 0.1f);
                     break;
 
             }
@@ -239,6 +283,19 @@ public class ExploreInFindControler : MonoBehaviour
 
 
     }
+    private void OnDisable()
+    {
+        //怪物失活
+
+        for (int j = 0; j < 18; j++)
+        {
+            if (!monsterDic.ContainsKey(MapNumber)||monsterDic[MapNumber][j].active==false) return;
+            monsterDic[MapNumber][j].SetActive(false);
+        }
+
+
+    }
+
     //WASD控制相机移动
     void CameraController()
     {
@@ -265,10 +322,171 @@ public class ExploreInFindControler : MonoBehaviour
     }
     void CameraMove(float x, float y, float z)
     {
-        relaCamera.transform.Translate(new Vector3(x, y, z), Space.Self);
+        camera.transform.Translate(new Vector3(x, y, z), Space.Self);
     }
 
+    /// <summary>
+    /// 怪物生成
+    /// </summary>
+    void SpawnMonster(int MapNumber)
+    {
+        if (MapNumber != 0)
+        {
+            switch (MapNumber)
+            {
+                case 1:
+                    if (monsterDic.ContainsKey(1))
+                    {
+                        for (int i = 0; i < 18; i++)
+                        {
+                            monsterDic[4][i].SetActive(true);
 
+                        }
+                        return;
+                    }
+                    List<GameObject> list1 = new List<GameObject>();
+                    for (int i = 0; i < 7; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[0], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        GameObject go1 = Instantiate(mapMonster[1], MonsterSpawnPos[7 + i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+
+                        go.name = "01";
+                        go1.name = "02";
+                        go.SetActive(true);
+                        go1.SetActive(true);
+                        list1.Add(go);
+                        list1.Add(go1);
+
+                    }
+                    for (int i = 14; i < 18; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[2], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.name = "03";
+                        go.SetActive(true);
+                        list1.Add(go);
+                    }
+                    //设怪物池为父物体
+                    for (int i = 14; i < 18; i++){
+                        list1[i].transform.SetParent(monsterPool.transform);
+                    }
+                    //加入字典
+                    monsterDic.Add(1, list1);
+                    break;
+
+                case 2:
+                    if (monsterDic.ContainsKey(2))
+                    {
+                        for (int i = 0; i < 18; i++)
+                        {
+                            monsterDic[4][i].SetActive(true);
+
+                        }
+                        return;
+                    }
+                    List<GameObject> list2 = new List<GameObject>();
+                    for (int i = 0; i < 7; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[2], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        GameObject go1 = Instantiate(mapMonster[3], MonsterSpawnPos[7 + i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.name = "03";
+                        go1.name = "04";
+                        go.SetActive(true);
+                        go1.SetActive(true);
+                        list2.Add(go);
+                        list2.Add(go1);
+                    }
+                    for (int i = 14; i < 18; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[4], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.name = "05";
+                        go.SetActive(true);
+                        list2.Add(go);
+                    }
+                    //设怪物池为父物体
+                    for (int i = 14; i < 18; i++){
+                        list2[i].transform.SetParent(monsterPool.transform);
+                    }
+                    monsterDic.Add(1, list2);
+                    break;
+                case 3:
+                    if (monsterDic.ContainsKey(3))
+                    {
+                        for (int i = 0; i < 18; i++)
+                        {
+                            monsterDic[4][i].SetActive(true);
+
+                        }
+                        return;
+                    }
+                    List<GameObject> list3 = new List<GameObject>();
+                    for (int i = 0; i < 7; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[4], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        GameObject go1 = Instantiate(mapMonster[5], MonsterSpawnPos[7 + i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.name = "05";
+                        go1.name = "06";
+                        go.SetActive(true);
+                        go1.SetActive(true);
+                        list3.Add(go);
+                        list3.Add(go1);
+
+                    }
+                    for (int i = 14; i < 18; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[6], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.name = "07";
+                        go.SetActive(true);
+                        list3.Add(go);
+                    }
+                    //设怪物池为父物体
+                    for (int i = 14; i < 18; i++){
+                        list3[i].transform.SetParent(monsterPool.transform);
+                    }
+                    monsterDic.Add(3, list3);
+                    break;
+                case 4:
+                    if (monsterDic.ContainsKey(4))
+                    {
+                        for (int i = 0; i < 18; i++)
+                        {
+                            monsterDic[4][i].SetActive(true);
+
+                        }
+                        return;
+                    }
+
+                    List<GameObject> list4 = new List<GameObject>();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[4], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        GameObject go1 = Instantiate(mapMonster[5], MonsterSpawnPos[5 + i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.SetActive(true);
+                        go1.SetActive(true);
+
+
+                        go.name = "05";
+                        go1.name = "06";
+                        list4.Add(go);
+                        list4.Add(go1);
+
+                    }
+
+                    for (int i = 10; i < 18; i++)
+                    {
+                        GameObject go = Instantiate(mapMonster[7], MonsterSpawnPos[i].transform.position, Quaternion.LookRotation(relaCamera.transform.position - transform.position));
+                        go.name = "08";
+                        go.SetActive(true);
+                        list4.Add(go);
+                    }
+                    //设怪物池为父物体
+                    for (int i = 14; i < 18; i++){
+                        list4[i].transform.SetParent(monsterPool.transform);
+                    }
+                    monsterDic.Add(4, list4);
+                    break;
+            }
+        }
+    }
 
 
 

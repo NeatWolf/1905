2e2ed6a5050cff72ee4269b2b023aa139849ea68
AYun-Neo <1096344703@qq.com>
2019-08-27 10:07:47 +0800

    遇怪委托

--- Assets/Script/CSharp/Animation/ExploreInFindControler.cs
+++ Assets/Script/CSharp/Animation/ExploreInFindControler.cs
@@ -4,12 +4,16 @@ using DragonBones;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
+using DG.Tweening;
 
 public class ExploreInFindControler : MonoBehaviour
 {
     NavMeshAgent mAgent;
     GameObject hero, monster, camera, relaCamera;
     UnityArmatureComponent uac;
+    /// <summary>
+    /// 是否在战斗状态
+    /// </summary>
     public bool isATK = false;
 
     [Header("选定的怪物")]
@@ -21,6 +25,22 @@ public class ExploreInFindControler : MonoBehaviour
     public UnityAction FindWayDone;
     [Header("战斗中禁止寻路警告")]
     public UnityAction WarringATKFindWay;
+    /// <summary>
+    /// 遇一级怪（小怪）委托
+    /// </summary>
+    public UnityAction LevelOneMonster;
+    /// <summary>
+    /// 遇二级怪（精英怪）委托
+    /// </summary>
+    public UnityAction LevelTwoMonster;
+    /// <summary>
+    /// 遇三级怪委托
+    /// </summary>
+    public UnityAction LevelThreeMonster;
+    /// <summary>
+    /// 遇四级怪（boss怪）委托
+    /// </summary>
+    public UnityAction LevelFourMonster;
 
 
     //英雄攻击距离，即寻路停止时与目标的距离
@@ -39,7 +59,8 @@ public class ExploreInFindControler : MonoBehaviour
     int cameraMoveType;
     //相机是否跟随英雄
     bool cameraTrack = true;
-    
+
+
 
     private void Awake()
     {
@@ -60,7 +81,7 @@ public class ExploreInFindControler : MonoBehaviour
 
     private void Update()
     {
-        
+
         //英雄看向相机
         for (int i = 0; i < heroTroop.Length; i++)
         {
@@ -81,10 +102,14 @@ public class ExploreInFindControler : MonoBehaviour
                 isIdle = false;
                 for (int i = 0; i < heroTroop.Length; i++)
                 {
-
+                    // if(targetPos.x>heroTroop[i].transform.position.x){
+                    //     heroTroop[i].transform.DORotate(new Vector3(0,180,0),0f);
+                    // }
+                    // if(targetPos.x<heroTroop[i].transform.position.x){
+                    //     heroTroop[i].transform.DORotate(new Vector3(0,0,0),0f);
+                    // }
                     heroAgent[i].SetDestination(targetPos);
                     heroAgent[i].stoppingDistance = heroAtkDistance;
-
                     herosUAC[i].animation.Play("run");
 
                 }
@@ -94,7 +119,7 @@ public class ExploreInFindControler : MonoBehaviour
             else if (Physics.Raycast(ray, out hitInfo, 1000, LayerMask.GetMask("ExploreMap")) && isATK == true)
             {
                 //调用禁止寻路警告方法
-                if(WarringATKFindWay==null) return;
+                if (WarringATKFindWay == null) return;
                 WarringATKFindWay();
             }
         }
@@ -106,7 +131,7 @@ public class ExploreInFindControler : MonoBehaviour
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo, 1000, LayerMask.GetMask("Monster")))
             {
-                CurrentMonster=hitInfo.transform.gameObject;
+                CurrentMonster = hitInfo.transform.gameObject;
                 cameraTrack = true;
                 targetPos = hitInfo.transform.position;
                 isIdle = false;
@@ -128,44 +153,51 @@ public class ExploreInFindControler : MonoBehaviour
 
             if (Vector3.Distance(heroTroop[i].transform.position, targetPos) <= heroAtkDistance)
             {
-                isIdle = true;
-
+                Debug.Log("播放idle动画");
+               // isIdle = true;
                 herosUAC[i].animation.Play("idle");
                 //调用遇怪方法
-                if(CurrentMonster!=null){
-                    FindWayDone();
+                if (CurrentMonster != null)
+                {
+                    if (FindWayDone != null)
+                    {
+                        FindWayDone();
+                    }
+                    if (CurrentMonster.name == "01" || CurrentMonster.name == "02")
+                    {
+                        if (LevelOneMonster == null) return;
+                        LevelOneMonster();
+                    }
+                    else if (CurrentMonster.name == "03" || CurrentMonster.name == "04")
+                    {
+                        if (LevelTwoMonster == null) return;
+                        LevelTwoMonster();
+                    }
+                    else if (CurrentMonster.name == "05" || CurrentMonster.name == "06")
+                    {
+                        if (LevelThreeMonster == null) return;
+                        LevelThreeMonster();
+                    }
+                    else if(CurrentMonster.name=="07"||CurrentMonster.name=="08"){
+                        if(LevelFourMonster==null) return;
+                        LevelFourMonster();
+                    }
+
+
                 }
 
             }
-            
-        }
-        
 
-        
+        }
+
+
+
 
 
 
 
     }
-    IEnumerator heroMove()
-    {
-        yield return null;
 
-        Debug.Log("run动画");
-
-        for (int i = 0; i < heroTroop.Length; i++)
-        {
-            herosUAC[i].animation.Play("run");
-        }
-
-        for (int i = 0; i < heroTroop.Length; i++)
-        {
-
-            herosUAC[i].animation.FadeIn("idle", 0.5f);
-        }
-
-
-    }
 
     //相机跟随
     private void LateUpdate()
@@ -193,10 +225,12 @@ public class ExploreInFindControler : MonoBehaviour
             switch (cameraMoveType)
             {
                 case 1:
-                    relaCamera.transform.Translate(0, Input.GetAxis("Vertical") * 0.1f, 0);
+                    // relaCamera.transform.Translate(0, Input.GetAxis("Vertical") * 0.1f, 0);
+                    CameraMove(0, Input.GetAxis("Vertical") * 0.1f, 0);
                     break;
                 case 2:
-                    relaCamera.transform.Translate(Input.GetAxis("Horizontal") * 0.1f, 0, 0);
+                    //  relaCamera.transform.Translate(Input.GetAxis("Horizontal") * 0.1f, 0, 0);
+                    CameraMove(Input.GetAxis("Horizontal") * 0.1f, 0, 0);
                     break;
 
             }
@@ -234,77 +268,9 @@ public class ExploreInFindControler : MonoBehaviour
         relaCamera.transform.Translate(new Vector3(x, y, z), Space.Self);
     }
 
-    /// <summary>
-    /// 多人寻路
-    /// </summary>
-    /// <param name="targetPos"></param>
-    /// <returns></returns>
-    IEnumerator FindWayMany(UnityArmatureComponent[] herosUAC, Vector3 targetPos)
-    {
-
-        for (int i = 0; i < herosUAC.Length; i++)
-        {
-            herosUAC[i].animation.Play("run");
-            heroAgent[i].SetDestination(targetPos);
-            heroAgent[i].stoppingDistance = heroAtkDistance;
-
-        }
-        yield return null;
-
-    }
-
-    //切换动画idle
-    IEnumerator FindWayAnimate(UnityArmatureComponent[] herosUAC, Vector3 targetPos)
-    {
-
-        yield return null;
-
-        for (int i = 0; i < herosUAC.Length; i++)
-        {
-            if (Vector3.Distance(heroTroop[i].transform.position, targetPos) <= heroAtkDistance)
-            {
-                //切换动画idle
-                for (int j = 0; j < herosUAC.Length; j++)
-                {
-                    herosUAC[i].animation.FadeIn("idle", 0.5f);
-                }
-                if (i == heroTroop.Length - 1 && FindWayDone != null)
-                {
-
-                    //调用寻路完成后的方法
-                    StartCoroutine(GoFindWayDone(FindWayDone));
-
-                }
-
-            }
-        }
-
-    }
-    //寻路完成后调用方法
-    IEnumerator GoFindWayDone(UnityAction action)
-    {
-        yield return new WaitForSeconds(0.5f);
-        if (FindWayDone != null)
-        {
-            FindWayDone();
-        }
-
-    }
-    //////////////////////////////////////////////////////
-    /// <summary>
-    /// 寻路
-    /// </summary>
-    /// <param name="targetPos"></param>
-    void FindWay(Vector3 targetPos)
-    {
-        for (int i = 0; i < herosUAC.Length; i++)
-        {
 
 
-            heroAgent[i].SetDestination(targetPos);
-            heroAgent[i].stoppingDistance = heroAtkDistance;
 
-        }
-    }
+
 
 }

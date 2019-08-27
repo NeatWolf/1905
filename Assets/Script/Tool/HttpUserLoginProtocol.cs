2d1e81b813182682aedbd0a76b9620ae07f10854
LYX <605074242@qq.com>
2019-08-27 15:44:19 +0800

    MakerCtrl LoginCtrl

---
+++ Assets/Script/Tool/HttpUserLoginProtocol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class HttpUserLoginProtocol
+{
+    [Serializable]
+    public class TokenData
+    {
+        public string Token;
+    }
+
+    public int Code;
+    public TokenData Data;
+}
